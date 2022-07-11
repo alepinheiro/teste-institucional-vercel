46cac4154da5d519850f30a5504bf656e530db08
BruEloiseHinrichs <bruna.hinrichs@seja.best>
2022-07-11 15:19:08 -0300

    nossos diferenciais

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -397,7 +397,7 @@
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Multibancos</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Multiprodutos</div>
@@ -407,42 +407,42 @@
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Transparência</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Agilidade no processo</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Atendimento 7ndias na semana</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Assessoria personalizada</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Totalmente Digital</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Menos burocracia</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Sem filas</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
         <div class="w-full flex border-b py-2  ">
           <div class="w-1/3 sm:w-2/4  mt-auto mb-auto text-center font-semibold text-textSecondary text-2xl sm:text-lg">Foco no cliente</div>
           <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.check"  class="w-12 h-auto mr-auto ml-auto"/></div>
-          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><img :src="$options.imageConfig.icons.error"  class="w-12 h-auto mr-auto ml-auto"/></div>
+          <div class="w-1/3 sm:w-1/4  mt-auto mb-auto"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 mr-auto ml-auto fill-textSecondary" viewBox="0 0 384 512"><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg></div>
         </div>
       </div>
     </div>
