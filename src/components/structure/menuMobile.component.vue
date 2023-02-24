76e9f52f5ff44db7a83b69d4cb5eca3ef1685237
Mayna (May) <maynnamartins@gmail.com>
2023-02-24 14:58:39 -0300

    função incompleta de click fora da div fechar a dropdown

--- src/components/structure/menuMobile.component.vue
+++ src/components/structure/menuMobile.component.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="flex mt-auto mb-auto justify-between">
-  <div @click="toggleMenu()" class="cursor-pointer" id="divMenu">
+<div class="flex mt-auto mb-auto justify-between"  >
+  <div @click="toggleMenu(), handleClickOutside($event)" class="cursor-pointer" id="divMenu">
     <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" :class="color" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z"/></svg>
   </div>
   <div v-if="openProfileMenu" class="w-full border-1 max-w-sm bgPopUpHover rounded-xl shadowDark z-40 fixed p-2 top-15 right-2 " id="menuMobile">
@@ -161,6 +161,18 @@ export default defineComponent({
     },
     toggleMenu(){
       this.openProfileMenu = !this.openProfileMenu;
+    },
+    handleClickOutside(event) {
+      const menu = document.querySelector('#menuMobile');
+      if (!menu.contains(event.target)) {
+         menu.classList.add('hidden')
+      }
+    },
+    mounted() {
+    window.addEventListener('click', this.handleClickOutside);
+    },
+    beforeDestroy() {
+      window.removeEventListener('click', this.handleClickOutside);
     }
   }
   
