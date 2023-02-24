f9b9d30288eaffbfa866b7424ee31e149cfa6f96
Mayna (May) <maynnamartins@gmail.com>
2023-02-24 11:10:56 -0300

    melhorias menuMobile

--- src/components/structure/menuMobile.component.vue
+++ src/components/structure/menuMobile.component.vue
@@ -5,10 +5,10 @@
   </div>
   <div v-if="openProfileMenu" class="w-full border-1 max-w-sm bgPopUpHover rounded-xl shadowDark z-40 fixed p-2 top-15 right-2 ">
         <!--Dropdown Mobile-->
-    <div class="flex flex-col justify-between" :class="{'optionsOpen': dropdownOpen}"  id="dropdown1"> 
+    <div class="flex flex-col justify-between mb-2" :class="{'optionsOpen': dropdownOpen}"  id="dropdown1"> 
       <button @click="toggleDropdown(), removeClass()" class=" cursor-pointer flex items-center px-4 py-2 justify-between"  :class="{'text-complementaryColor1': dropdownOpen }">
         Para você
-      <svg :class="['w-4 h-4 ml-2', dropdownOpen ? 'transform rotate-180' : '']" viewBox="0 0 20 20" fill="currentColor">
+      <svg :class="['w-6 h-6 ml-2', dropdownOpen ? 'transform rotate-180' : '']" viewBox="0 0 20 20" fill="currentColor">
         <path
           fill-rule="evenodd"
           d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -16,7 +16,7 @@
         />
       </svg>
       </button>
-      <div  class=" py-2 px-4 shadow-lg" :class="{hidden: !dropdownOpen}">
+      <div  class=" py-2 ml-2 px-4 shadow-lg" :class="{hidden: !dropdownOpen}">
         <ul class="flex flex-col space-y-2 ">
         <li class="w-full flex flex-row justify-between items-center" @mouseover="hover1 = true"  @mouseout="hover1 = false">
           <router-link :to="'/financiamento-imobiliario'+this.$root.utms" class="block text-textPrimary py-1 hover:font-semibold" :class="{'text-primary': hover1}" > Financiamento </router-link>
@@ -40,10 +40,10 @@
       </div>
     </div>
     <!--Dropdown Mobile-->
-    <div class="flex flex-col" :class="[ dropdown2  ? 'optionsOpen': '']" id="dropdown2"> 
-      <button @click="toggleDropdown2(),  removeClassDropdown2()" @mouseout="" class=" cursor-pointer flex items-center px-4 py-2 justify-between" >
+    <div class="flex flex-col mb-2" :class="[ dropdown2  ? 'optionsOpen': '']" id="dropdown2"> 
+      <button @click="toggleDropdown2(),  removeClassDropdown2()" @mouseout="" class=" cursor-pointer flex items-center px-4 py-2 mb-2 justify-between" >
         Para o seu negócio
-      <svg :class="['w-4', 'h-4', 'ml-2', {'transform': dropdown2, 'rotate-180': dropdown2, 'rotate-0': dropdownOpen}]" viewBox="0 0 20 20" fill="currentColor">
+      <svg :class="['w-6', 'h-6', 'ml-2', {'transform': dropdown2, 'rotate-180': dropdown2, 'rotate-0': dropdownOpen}]" viewBox="0 0 20 20" fill="currentColor">
         <path
           fill-rule="evenodd"
           d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -82,21 +82,21 @@
         </ul>
       </div>
     </div>
-    <div @click="openProfileMenu= !openProfileMenu" class=" cursor-pointer rounded-lg px-4 py-2 optionsHover flex flex-row justify-between" @mouseover="hoverSVG1 = true" @mouseout="hoverSVG1= false">
+    <div @click="openProfileMenu= !openProfileMenu" class=" cursor-pointer rounded-lg px-4 py-2 mb-2 optionsHover flex flex-row justify-between" @mouseover="hoverSVG1 = true" @mouseout="hoverSVG1= false">
       <router-link :to="'/parceiro'+this.$root.utms" class=" hover:font-semibold " > Seja um Parceiro </router-link>
       <svg v-show="hoverSVG1" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 opacity-o group-hover:opacity-100 " viewBox="0 0 20 20" fill="currentColor ">
           <path fill-rule="evenodd" d="M14.293 10.293a1 1 0 0 0 0-1.414L10.414 5.586a1 1 0 1 0-1.414 1.414L11.586 9H3a1 1 0 0 0 0 2h8.586l-3.707 3.707a1 1 0 1 0 1.414 1.414L14.293 11.7a1 1 0 0 0 0-1.414z" clip-rule="evenodd" />
       </svg>
     </div>
 
-    <div @click="openProfileMenu= !openProfileMenu" class=" cursor-pointer rounded-lg px-4 py-2 optionsHover flex flex-row justify-between" @mouseover="hoverSVG2 = true" @mouseout="hoverSVG2 = false">
+    <div @click="openProfileMenu= !openProfileMenu" class=" cursor-pointer rounded-lg px-4 py-2 mb-2 optionsHover flex flex-row justify-between" @mouseover="hoverSVG2 = true" @mouseout="hoverSVG2 = false">
       <a :href="'https://portal.seja.best/'+this.$root.utms"  style=" text-decoration: none;"  class=" hover:font-semibold "
         target="_blank"> Portal</a>
         <svg v-show="hoverSVG2" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 opacity-o group-hover:opacity-100 " viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M14.293 10.293a1 1 0 0 0 0-1.414L10.414 5.586a1 1 0 1 0-1.414 1.414L11.586 9H3a1 1 0 0 0 0 2h8.586l-3.707 3.707a1 1 0 1 0 1.414 1.414L14.293 11.7a1 1 0 0 0 0-1.414z" clip-rule="evenodd" />
         </svg>
     </div>
-    <div @click="openProfileMenu= !openProfileMenu" class=" cursor-pointer rounded-lg px-4 py-2 optionsHover flex flex-row justify-between" @mouseover="hoverSVG3 = true" @mouseout="hoverSVG3=false">
+    <div @click="openProfileMenu= !openProfileMenu" class=" cursor-pointer rounded-lg px-4 py-2 mb-2 optionsHover flex flex-row justify-between" @mouseover="hoverSVG3 = true" @mouseout="hoverSVG3=false">
       <router-link :to="'/sobre'+this.$root.utms" class="hover:font-semibold " > Sobre </router-link>
       <svg v-show="hoverSVG3" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 opacity-o group-hover:opacity-100 " viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M14.293 10.293a1 1 0 0 0 0-1.414L10.414 5.586a1 1 0 1 0-1.414 1.414L11.586 9H3a1 1 0 0 0 0 2h8.586l-3.707 3.707a1 1 0 1 0 1.414 1.414L14.293 11.7a1 1 0 0 0 0-1.414z" clip-rule="evenodd" />
