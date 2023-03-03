8c5183705177e8f7458b355073c423681533f8f7
Mayna (May) <maynnamartins@gmail.com>
2023-03-03 15:16:02 -0300

    O nome do roadape foi atualizado espaço lateral no final da home melhorado

--- src/views/HomeEquity.vue
+++ src/views/HomeEquity.vue
@@ -531,7 +531,7 @@
 import { defineComponent } from 'vue';
 import popUpVideo from "@/components/base/popUpVideo.component.vue"
 import topbarComponent from '@/components/structure/topbar.component.vue';
-import rodape from '@/components/base/rodape.vue';
+import rodape from '@/components/base/bannerBottomPage.vue';
 export default defineComponent({
   name: 'HomeEquity',
   components:{
