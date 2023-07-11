2e3b2ff62e9807050fb2ef114e65db1db5c11029
Ale Pinheiro <alessandropsbra@gmail.com>
2023-07-11 15:53:24 -0300

    add footer section

---
+++ src/components/bestPro/footerSection.component.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="bg-black">
+    FooterSection
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'FooterSection',
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="scss"></style>
---
+++ src/views/BestProLP.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="text-white">
+    <FooterSection />
+  </main>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import FooterSection from '@/components/bestPro/footerSection.component.vue'
+export default defineComponent({
+  name: 'BestProLP',
+  components: { FooterSection },
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="scss"></style>
