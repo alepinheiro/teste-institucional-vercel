5e4337ebe4746f5f37f94e04ec11ec494e997ce6
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-09-11 16:09:22 -0300

    create files

---
+++ src/views/BestBrokerLP.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col">
+        <section></section>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'BestBrokerLp',
+  data() {
+    return {}
+  },
+})
+</script>
