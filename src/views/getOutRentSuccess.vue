e6912da54b2c974e49859545e2cff731e38c8dfe
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-09-18 16:34:11 -0300

    add page view

---
+++ src/views/getOutRentSuccess.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        adquirido
+    </div>
+</template>
+<script lang='ts'>
+import { defineComponent } from 'vue';
+export default defineComponent({
+name: 'XoAluguelAdquirido',
+data(){
+return {}
+}
+})
+</script>
