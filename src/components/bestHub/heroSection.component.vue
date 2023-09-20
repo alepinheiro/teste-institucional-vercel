4d8c67eeebcb25dcddd0babff64897037cbdeb9a
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-09-20 17:34:57 -0300

    update hero video

--- src/components/bestHub/heroSection.component.vue
+++ src/components/bestHub/heroSection.component.vue
@@ -1,20 +1,31 @@
 <template>
   <section class="w-full">
     <div class="w-full min-h-screen relative z-0 flex">
-      <img src="/images/bestHub/heroBackground.png" class="absolute object-cover h-full w-full -z-10" alt="" srcset="">
-      <!-- <iframe
+      <!-- <img src="/images/bestHub/heroBackground.png" class="absolute object-cover h-full w-full -z-10" alt="" srcset=""> -->
+      <iframe
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/A6cmeg6s0Hg?autoplay=1&mute=1"
+        src="https://www.youtube.com/embed/NmVcwxwQ2mk?autoplay=1&mute=1&loop=1&playlist=NmVcwxwQ2mk"
         title="Quais são as diferenças entre financiamento imobiliário do banco e da construtora?"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
-        class="absolute sm:-top-[15%] inset-0 -z-10"
+        class="absolute sm:-top-[0%] inset-0 -z-10 -top-14 sm:hidden"
       >
-      </iframe> -->
+      </iframe>
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/SdZZsOFr4zw?autoplay=1&mute=1&loop=1&playlist=SdZZsOFr4zw"
+        title="Quais são as diferenças entre financiamento imobiliário do banco e da construtora?"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+        class="absolute sm:-top-[15%] inset-0 -z-10 -top-14 hidden sm:block"
+      >
+      </iframe>
       <div
-        class="w-full h-auto flex-grow bg-gradient-to-b from-transparent to-black flex items-center pb-10"
+        class="w-full h-auto flex-grow bg-gradient-to-b from-transparent via-black/70 to-90% to-black flex items-center pb-10"
       >
         <div
           class="flex flex-col max-w-3xl sm:px-5 items-center mx-auto text-center mt-auto pb-20"
