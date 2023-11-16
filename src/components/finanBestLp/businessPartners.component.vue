1229f02632377a5d9044390d0472b486a8b1849c
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-11-16 16:21:21 -0300

    update businessPartners md

--- src/components/finanBestLp/businessPartners.component.vue
+++ src/components/finanBestLp/businessPartners.component.vue
@@ -1,26 +1,29 @@
 <template>
   <section class="w-full flex py-10">
     <div
-      class="flex flex-col gap-6 max-w-7xl mx-auto font-Public-Sans text-center"
+      class="flex flex-col gap-6 max-w-7xl md:max-w-2xl mx-auto font-Public-Sans text-center px-5"
     >
       <h2 class="text-3xl font-bold text-primary">
         Na SejaBest, suas chances de aprovação são maiores
       </h2>
-      <p class="text-xl text-textPrimary">
+      <p class="text-xl text-textPrimary md:text-justify">
         Com a SejaBest, você tem acesso a produtos de mais de 20 instituições
         financeiras, o que pode lhe garantir as melhores taxas e as maiores
         chances de aprovação, além de um time de especialistas preparado para te
         atender e te guiar durante todo o processo.
       </p>
-      <div class="flex flex-row">
-        <img
-          src="/images/finanBest/businessPartnersXL161123.png"
-          alt=""
-          srcset=""
-        />
-        <div class="flex-wrap flex flex-row gap-5 justify-center">
-          <div v-for="{ id, alt, source } of banks" :key="id" class="bg-black/5 py-3 px-4 rounded-md h-fit">
-            <img :src="source" :alt="alt" class="h-8" />
+      <div class="flex flex-row md:flex-col md:items-center md:gap-4 items-end">
+        <div class="flex-1">
+          <img
+            src="/images/finanBest/businessPartnersXL161123.png"
+            alt=""
+            srcset=""
+            class="object-contain"
+          />
+        </div>
+        <div class="flex-wrap flex flex-row gap-5 lg:gap-2 justify-center flex-1">
+          <div v-for="{ id, alt, source } of banks" :key="id" class="bg-black/5 py-3 px-4 rounded-md">
+            <img :src="source" :alt="alt" class="h-8 lg:h-6 md:h-6" />
           </div>
         </div>
       </div>
