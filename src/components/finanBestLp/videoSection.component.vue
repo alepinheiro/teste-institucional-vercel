0362a8ab485ddf0afa1a922dc54c2a6c154e0ae9
Ale Pinheiro <alessandropsbra@gmail.com>
2023-11-17 17:46:38 -0300

    update video section

--- src/components/finanBestLp/videoSection.component.vue
+++ src/components/finanBestLp/videoSection.component.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="py-10 w-full bg-[#EFEFEF]">
-    <div class="max-w-7xl lg:max-w-5xl md:max-w-2xl w-full flex flex-row items-center gap-3 mx-auto px-5">
-      <div class="w-5/12 lg:w-7/12 md:w-full flex flex-col gap-8 text-textPrimary text-center">
-        <h2 class="text-3xl text-primary font-bold">
+    <div class="max-w-7xl lg:max-w-5xl md:max-w-2xl w-full flex flex-row sm:flex-col items-center gap-3 mx-auto px-5">
+      <div class="w-5/12 lg:w-7/12 md:w-full sm:w-full flex flex-col gap-8 text-textPrimary text-center">
+        <h2 class="text-3xl sm:text-xl text-primary font-bold">
           O FinanBest é o produto de financiamento imobiliário da SejaBest.
         </h2>
-        <div class="text-xl leading-relaxed">
+        <div class="text-xl leading-relaxed sm:hidden">
           <p>
             Imagine financiar seu novo imóvel sem sair de casa, contando com um
             time de especialistas prontos para lhe atender a qualquer momento.
@@ -19,7 +19,7 @@
           </p>
         </div>
       </div>
-      <div v-show="showVideo" class="w-7/12 lg:w-5/12 h-96 rounded-2xl overflow-hidden md:hidden">
+      <div v-show="showVideo" class="w-7/12 lg:w-5/12 sm:w-full h-96 sm:h-48 rounded-2xl overflow-hidden md:hidden">
         <iframe
           src="https://www.youtube.com/embed/IXZj925aJDY"
           title="Como financiar meu primeiro imóvel?"
@@ -29,11 +29,11 @@
           class="h-full w-full"
         ></iframe>
       </div>
-      <div v-show="!showVideo" class="w-7/12 lg:w-5/12 relative bg-black rounded-2xl z-0 md:hidden overflow-hidden">
+      <div v-show="!showVideo" class="w-7/12 lg:w-5/12 sm:w-full relative bg-black rounded-2xl z-0 md:hidden overflow-hidden">
         <img
           src="/images/finanBest/bgVideoPlayer.png"
           alt=""
-          class="opacity-50 object-cover h-96 w-full"
+          class="opacity-50 object-cover h-96 sm:h-48 w-full"
         />
         <div
           class="w-8 h-8 rounded-full absolute inset-0 m-auto animate-ping z-0 bg-zinc-300"
@@ -47,6 +47,19 @@
           </button>
         </div>
       </div>
+      <div class="text-sm text-center leading-relaxed sm:block hidden">
+          <p>
+            Imagine financiar seu novo imóvel sem sair de casa, contando com um
+            time de especialistas prontos para lhe atender a qualquer momento.
+          </p>
+          <br />
+          <p>
+            <b>
+              Assista o vídeo que preparamos especialmente para você e entenda
+              cada detalhe desta operação.
+            </b>
+          </p>
+        </div>
     </div>
   </section>
 </template>
