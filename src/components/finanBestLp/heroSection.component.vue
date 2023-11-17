3bd812115da036e928e2b078d9b8248ee6b6f6c9
Ale Pinheiro <alessandropsbra@gmail.com>
2023-11-17 17:28:03 -0300

    update hero section

--- src/components/finanBestLp/heroSection.component.vue
+++ src/components/finanBestLp/heroSection.component.vue
@@ -1,24 +1,24 @@
 <template>
-  <section class="bg-primary w-full flex py-10 relative md:min-h-screen">
-    <div class="absolute inset-0 flex justify-center z-20">
+  <section class="bg-primary w-full flex pt-10 relative md:min-h-screen sm:min-h-screen">
+    <div class="absolute inset-x-0 h-fit top-0 flex justify-center z-20">
       <Topbar class=" w-full max-w-7xl lg:max-w-6xl px-5 mb-auto pt-4" />
     </div>
     <div class="absolute inset-0 overflow-hidden z-0 flex items-end">
       <img src="/images/finanBest/heroSectionXL161123.png" alt="" class="hidden xl:block lg:block object-cover h-full ml-auto">
-      <img src="/images/finanBest/heroSectionMD161123.png" alt="" class="hidden md:block object-cover h-full w-full">
+      <img src="/images/finanBest/heroSectionMD161123.png" alt="" class="hidden md:block sm:block object-cover h-full w-full">
     </div>
-    <div class="max-w-7xl md:max-w-2xl mx-auto flex flex-row w-full z-10 pt-24 px-5 relative">
+    <div class="max-w-7xl md:max-w-2xl mx-auto flex flex-row w-full z-10 pt-24 px-5 sm:px-0 relative">
       <!--  -->
-      <div class="flex-1 md:mt-auto">
+      <div class="flex-1 md:mt-auto sm:bg-gradient-to-b sm:from-transparent sm:to-bgDarkColor">
         <div
-          class="bg-white flex flex-col gap-6 md:gap-3 font-Public-Sans p-8 md:px-8 py-4 rounded-xl shadow-lg"
+          class="bg-white sm:bg-transparent flex flex-col gap-6 sm:gap-2 md:gap-3 font-Public-Sans p-8 md:px-8 py-4 rounded-xl shadow-lg sm:h-full sm:justify-end"
         >
-          <h1 class="text-5xl md:text-4xl text-primary leading-tight text-justify">
+          <h1 class="text-5xl sm:text-lg sm:text-white md:text-4xl text-primary leading-tight text-justify sm:drop-shadow">
             <strong>FinanBest:</strong>
             Financie seu imóvel com a assessoria
             <strong>SejaBest</strong>
           </h1>
-          <p class="text-lg md:text-base text-justify">
+          <p class="text-lg sm:text-xs sm:text-white md:text-base text-justify sm:leading-loose sm:drop-shadow">
             Financie seu imóvel com as melhores condições do mercado.
             <b
               >Sem filas, sem gerentes, sem complexidade e com um time de
@@ -28,12 +28,12 @@
           <InputWithSlider :slider-props="sliderProps" />
           <div class="flex flex-row gap-2 text-textPrimary items-center md:mx-auto">
             <i class="fa-solid fa-chevron-down"></i>
-            <p class="text-xs ">Ou saiba mais sobre o Financiamento Imobiliário da SejaBest rolando para baixo</p>
+            <p class="text-xs sm:text-2xs">Ou saiba mais sobre o Financiamento Imobiliário da SejaBest rolando para baixo</p>
           </div>
         </div>
       </div>
       <!--  -->
-      <div class="flex-1 md:hidden"></div>
+      <div class="flex-1 md:hidden sm:hidden"></div>
       <!--  -->
     </div>
   </section>
--- src/components/finanBestLp/inputWithSlider.component.vue
+++ src/components/finanBestLp/inputWithSlider.component.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="bg-primary flex flex-col gap-6 md:gap-3 rounded-xl p-4">
-      <label for="amountInput" class="text-xl text-white font-bold">
+    <div class="bg-primary sm:bg-white/20 flex flex-col gap-6 sm:gap-3 md:gap-3 rounded-xl p-4">
+      <label for="amountInput" class="text-xl text-white font-bold sm:text-center sm:text-md">
         Quanto custa o seu sonho?
       </label>
       <div class="flex flex-row gap-4">
@@ -11,11 +11,11 @@
           v-model="inputValue"
           name="amountInput"
           type="text"
-          class="rounded-md w-full px-4"
+          class="rounded-md w-full px-4 sm:py-2"
           placeholder="R$ 250.000,00"
           @input="onInput"
         />
-        <button class="bg-complementaryColor1 text-white px-4 py-2 rounded-md">
+        <button class="bg-complementaryColor1 text-white px-4 py-2 rounded-md sm:hidden">
           Simular
         </button>
       </div>
@@ -29,6 +29,9 @@
         class="slider"
         @input="setValue(numberValue)"
       />
+      <button class="bg-complementaryColor1 text-white px-4 py-2 rounded-md hidden sm:block w-fit mx-auto">
+          Simular
+        </button>
     </div>
   </div>
 </template>
