181fbff72d1e9ccadda341abbce9a8f397dfbc4d
Ale Pinheiro <alessandropsbra@gmail.com>
2023-11-19 23:17:36 -0300

    add acessibility eslint

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import { register } from 'swiper/element/bundle'
+
+register()
 library.add(fas, far, fab)
 dom.watch()
 
