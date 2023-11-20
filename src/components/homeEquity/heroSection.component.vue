f87238c92aeb68315770e917ff1a184481ffdc46
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-11-20 15:48:03 -0300

    add new page

---
+++ src/components/homeEquity/heroSection.component.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="bg-bgDarkColor w-full flex pt-10 relative min-h-screen sm:pt-0">
+    <div class="absolute inset-x-0 h-fit top-0 flex justify-center z-20">
+      <Topbar class="w-full max-w-7xl lg:max-w-5xl px-5 mb-auto pt-4" />
+    </div>
+  </section>
+</template>
+<script lang="ts" setup>
+import Topbar from '@/components/structure/topbar.component.vue'
+</script>
---
+++ src/views/newHomeEquityLP.vue
@@ -0,0 +1,8 @@
+<template>
+    <main>
+        <HeroSection />
+    </main>
+</template>
+<script lang='ts' setup>
+import HeroSection from '@/components/homeEquity/heroSection.component.vue'
+</script>
