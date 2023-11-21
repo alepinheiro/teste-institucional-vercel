ef4f3cea6b4f1c4a4b8e5d5a97b2bb86b3290641
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-11-21 11:22:22 -0300

    add steps section

---
+++ src/components/cashBest/stepsSection.component.vue
@@ -0,0 +1,90 @@
+<template>
+  <section class="py-10 bg-[#EFEFEF]">
+    <div class="xl:max-w-7xl mx-auto flex flex-col gap-10 px-5">
+      <div class="flex flex-row w-full justify-between items-center">
+        <div class="flex flex-col gap-1">
+          <h2 class="text-3xl text-primary font-bold">
+            Crédito sem filas, nem gerentes, nem complexidade
+          </h2>
+          <p class="text-sm text-textPrimary">
+            São apenas 4 passos para você receber o seu crédito.
+          </p>
+        </div>
+        <div>
+          <img src="/images/logoBest/logoCashBestHomeEquity.svg" alt="" />
+        </div>
+      </div>
+      <component
+        :is="'swiper-container'"
+        v-bind="sliderOptions"
+        class="w-full lg:pl-40"
+      >
+        <component
+          :is="'swiper-slide'"
+          v-for="{ id, icon, title, description, callToAction } of stepCards"
+          :key="id"
+          class="bg-white rounded-lg p-12 h-auto w-80 lg:w-80 md:w-72 mb-10"
+        >
+          <div class="flex flex-col gap-12 items-start font-Public-Sans">
+            <i :class="icon" class="text-primary w-5 h-5"></i>
+            <h3 class="text-2xl font-bold">{{ id }}. {{ title }}</h3>
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <p v-html="description"></p>
+            <a v-if="callToAction" :href="callToAction[1]" class="underline">
+              {{ callToAction[0] }}
+            </a>
+          </div>
+        </component>
+      </component>
+    </div>
+  </section>
+</template>
+<script setup lang="ts">
+import { SwiperOptions } from 'swiper/types'
+
+const sliderOptions: SwiperOptions = {
+  spaceBetween: 20,
+  slidesPerView: 'auto',
+  pagination: true,
+  breakpoints: {
+    1920: {
+      slidesPerView: 4,
+    },
+  },
+}
+
+const stepCards = [
+  {
+    id: 1,
+    icon: 'fa-solid fa-layer-group',
+    title: 'Simule',
+    description:
+      'A simulação é feita através do nosso simulador. <b>Em até 20 minutos você tem o resultado, porque seu tempo é valioso.</b>',
+    callToAction: ['Simule agora', '#'],
+  },
+  {
+    id: 2,
+    icon: 'fa-solid fa-file',
+    title: 'Documentação',
+    description:
+      'Para uma análise de crédito detalhada, envie os documentos solicitados. <b>Então seus dados serão analisados e, em seguida, uma proposta será enviada.</b>',
+    callToAction: ['Baixe o checklist', '#'],
+  },
+  {
+    id: 3,
+    icon: 'fa-solid fa-search',
+    title: 'Avaliação e jurídico',
+    description:
+      'Nesta etapa, um engenheiro avalia quanto o imóvel vale. Além disso, é feita uma análise para conferir a <b>situação jurídica da propriedade e validar o bem como uma garantia.</b>',
+    callToAction: null,
+  },
+  {
+    id: 4,
+    icon: 'fa-solid fa-circle-dollar-to-slot',
+    title: 'Liberação',
+    description:
+      'Agora a documentação é encaminhada para o cartório e você assina os documentos digitalmente. <b>O seu bem é então alienado e seu recurso liberado na sua conta!</b>',
+    callToAction: null,
+  },
+]
+</script>
--- src/views/CashBestLP.vue
+++ src/views/CashBestLP.vue
@@ -10,7 +10,8 @@
       :cards="{ bgColor: 'SECONDARY' }"
       :container="{ bgColor: 'BGDARKCOLOR' }"
     />
-    <SolutionsSection />
+    <Solutions />
+    <Steps />
     <TheFooter />
   </main>
 </template>
@@ -20,7 +21,8 @@ import HeroSection from '@/components/cashBest/heroSection.component.vue'
 import BusinessPartners from '@/components/base/businessPartners.component.vue'
 import ComparisonTable from '@/components/finanBestLp/comparisonTable.component.vue'
 import AboutUs from '@/components/finanBestLp/aboutUs.component.vue'
-import SolutionsSection from '@/components/cashBest/solutionsSection.component.vue'
+import Solutions from '@/components/cashBest/solutionsSection.component.vue'
+import Steps from '@/components/cashBest/stepsSection.component.vue'
 import TheFooter from '@/components/structure/theFooter.component.vue'
 useHead({
   title: 'CashBest: o Crédito com Garantia de Imóvel da SejaBest',
