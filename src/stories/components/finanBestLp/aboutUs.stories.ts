ece76faf29165b8a2479efb518d1478f565d4482
Ale Pinheiro <alessandropsbra@gmail.com>
2023-11-24 11:17:26 -0300

    add abouUs Component

---
+++ src/stories/components/finanBestLp/aboutUs.stories.ts
@@ -0,0 +1,50 @@
+import AboutUs from '@/components/finanBestLp/aboutUs.component.vue'
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { vueRouter } from 'storybook-vue3-router'
+import type { ComponentProps } from 'vue-component-type-helpers'
+
+type CustomProps = ComponentProps<typeof AboutUs> & {
+  bgCard: { bgColor: 'PRIMARY' | 'SECONDARY' | 'BGDARKCOLOR' }
+  bgContainer: { bgColor: 'PRIMARY' | 'SECONDARY' | 'BGDARKCOLOR' }
+}
+
+const meta = {
+  title: 'Components/FinanBestLp/AboutUs',
+  component: AboutUs,
+  tags: ['autodocs'],
+  argTypes: {
+    bgContainer: {
+      control: 'select',
+      options: ['PRIMARY', 'SECONDARY', 'BGDARKCOLOR'],
+    },
+    bgCard: {
+      control: 'select',
+      options: ['PRIMARY', 'SECONDARY', 'BGDARKCOLOR'],
+    },
+  },
+  args: {
+    bgContainer: { bgColor: 'BGDARKCOLOR' },
+    bgCard: { bgColor: 'BGDARKCOLOR' },
+  },
+  render: (args) => ({
+    components: { AboutUs },
+    setup() {
+      return {
+        cards: { bgColor: args.bgCard },
+        container: { bgColor: args.bgContainer },
+      }
+    },
+    template: '<AboutUs :cards="cards" :container="container" />',
+  }),
+} satisfies Meta<CustomProps>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    cards: meta.args.bgCard,
+    container: meta.args.bgContainer,
+  },
+  decorators: [vueRouter()],
+}
