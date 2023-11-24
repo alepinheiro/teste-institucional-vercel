2334a39d786be10059b8399429010d7ec65ffb98
Ale Pinheiro <alessandropsbra@gmail.com>
2023-11-24 12:59:33 -0300

    add chashbest product story

--- src/stories/components/cashBestLp/theProduct.stories.ts
+++ src/stories/components/cashBestLp/theProduct.stories.ts
@@ -1,12 +1,12 @@
-import Steps from '@/components/cashBest/stepsSection.component.vue'
+import Product from '@/components/cashBest/theProduct.component.vue'
 import type { Meta, StoryObj } from '@storybook/vue3'
 import { vueRouter } from 'storybook-vue3-router'
 
 const meta = {
-  title: 'Components/CashBestLp/Steps',
-  component: Steps,
+  title: 'Components/CashBestLp/Product',
+  component: Product,
   tags: ['autodocs'],
-} satisfies Meta<typeof Steps>
+} satisfies Meta<typeof Product>
 
 export default meta
 type Story = StoryObj<typeof meta>
