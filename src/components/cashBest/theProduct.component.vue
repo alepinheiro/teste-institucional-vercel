fa29738d9013db93d8c5aef9a0c63d1f80e87f41
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-11-29 15:13:23 -0300

    product update utms

--- src/components/cashBest/theProduct.component.vue
+++ src/components/cashBest/theProduct.component.vue
@@ -1,29 +1,50 @@
 <template>
   <section class="py-20">
-    <div class="flex flex-row md:flex-col sm:flex-col items-center xl:max-w-7xl lg:max-w-5xl md:max-w-2xl md:gap-6 mx-auto px-5">
+    <div
+      class="flex flex-row md:flex-col sm:flex-col items-center xl:max-w-7xl lg:max-w-5xl md:max-w-2xl md:gap-6 mx-auto px-5"
+    >
       <div class="flex flex-col gap-6 sm:gap-3 flex-1">
         <h2 class="text-3xl text-primary font-bold sm:text-xl">
           O crédito mais barato do mercado.
         </h2>
-        <p class="text-textPrimary w-5/6 md:w-full md:text-justify sm:w-full sm:text-sm sm:text-justify">
+        <p
+          class="text-textPrimary w-5/6 md:w-full md:text-justify sm:w-full sm:text-sm sm:text-justify"
+        >
           <b>
             O empréstimo com garantia de imóvel é a opção mais econômica
-          disponível no mercado.
+            disponível no mercado.
           </b>
-           A razão é simples: ao usar seu imóvel como
-          garantia, você desfruta das <u>taxas mais baixas possíveis.</u>  Isso
-          significa que você pode obter o crédito de que precisa com custos
-          realmente competitivos.
+          A razão é simples: ao usar seu imóvel como garantia, você desfruta das
+          <u>taxas mais baixas possíveis.</u> Isso significa que você pode obter
+          o crédito de que precisa com custos realmente competitivos.
         </p>
-        <hr class="hidden sm:block border-zinc-400">
-        <a href="#" class="font-bold text-black bg-complementaryColor1 rounded px-4 py-2 w-fit sm:hidden">Simule on-line de graça</a>
+        <hr class="hidden sm:block border-zinc-400" />
+        <a
+          :href="`https://simulador.seja.best/?${utm.toString()}`"
+          class="font-bold text-black bg-complementaryColor1 rounded px-4 py-2 w-fit sm:hidden"
+          >Simule on-line de graça</a
+        >
       </div>
       <div class="flex flex-1 md:mx-auto">
-        <img src="/images/cashBest/marketTax.svg" alt="Veja e compare as taxas médias do mercado" srcset="" class="ml-auto md:mx-auto h-96">
+        <img
+          src="/images/cashBest/marketTax.svg"
+          alt="Veja e compare as taxas médias do mercado"
+          srcset=""
+          class="ml-auto md:mx-auto h-96"
+        />
       </div>
-      <a href="#" class="font-bold text-black bg-complementaryColor1 rounded px-4 py-2 w-fit hidden sm:block">Simule on-line de graça</a>
-
+      <a
+        :href="`https://simulador.seja.best/?${utm.toString()}`"
+        class="font-bold text-black bg-complementaryColor1 rounded px-4 py-2 w-fit hidden sm:block"
+        >Simule on-line de graça</a
+      >
     </div>
   </section>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const utm = new URLSearchParams({
+  utm_source: 'institucional-seja-best',
+  utm_medium: 'home-equity-taxes-cashbest',
+  utm_campaign: 'landing-pages-dez-23',
+})
+</script>
