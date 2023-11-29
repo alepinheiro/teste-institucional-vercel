ffb2b18d170ce47352b0077acea61354be072cc2
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-11-29 15:15:58 -0300

    update theProduct texts size

--- src/components/cashBest/theProduct.component.vue
+++ src/components/cashBest/theProduct.component.vue
@@ -3,12 +3,12 @@
     <div
       class="flex flex-row md:flex-col sm:flex-col items-center xl:max-w-7xl lg:max-w-5xl md:max-w-2xl md:gap-6 mx-auto px-5"
     >
-      <div class="flex flex-col gap-6 sm:gap-3 flex-1">
-        <h2 class="text-3xl text-primary font-bold sm:text-xl">
+      <div class="flex flex-col gap-6 sm:gap-8 flex-1">
+        <h2 class="text-5xl text-primary font-bold">
           O crédito mais barato do mercado.
         </h2>
         <p
-          class="text-textPrimary w-5/6 md:w-full md:text-justify sm:w-full sm:text-sm sm:text-justify"
+          class="text-textPrimary w-5/6 md:w-full md:text-justify sm:w-full text-base sm:text-justify"
         >
           <b>
             O empréstimo com garantia de imóvel é a opção mais econômica
@@ -25,7 +25,7 @@
           >Simule on-line de graça</a
         >
       </div>
-      <div class="flex flex-1 md:mx-auto">
+      <div class="flex flex-1 md:mx-auto sm:pt-8">
         <img
           src="/images/cashBest/marketTax.svg"
           alt="Veja e compare as taxas médias do mercado"
@@ -35,7 +35,7 @@
       </div>
       <a
         :href="`https://simulador.seja.best/?${utm.toString()}`"
-        class="font-bold text-black bg-complementaryColor1 rounded px-4 py-2 w-fit hidden sm:block"
+        class="font-bold text-black bg-complementaryColor1 rounded px-4 py-2 w-fit hidden sm:block sm:mt-8"
         >Simule on-line de graça</a
       >
     </div>
