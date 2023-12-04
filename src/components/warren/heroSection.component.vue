d5cc36b618f7f3e8b2b8eabe0ffac5e6dff97a4f
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-12-04 14:48:20 -0300

    update hero section css

--- src/components/warren/heroSection.component.vue
+++ src/components/warren/heroSection.component.vue
@@ -1,14 +1,26 @@
 <template>
-  <section class="w-full ">
-    <div class="px-5 flex flex-col gap-6 items-center pt-18 pb-40 relative z-0">
-      <img src="/images/warren/mobile/amanhecer@5x.png" class="absolute -z-10 top-0 h-full object-fill" alt="background" role="presentation" />
-			<img src="/images/warren/logoWarrenWhite.svg" alt="" srcset="">
-      <h1 class="text-3xl font-bold text-center text-white">Fazer crédito toma tempo, mas não precisa ser o seu</h1>
-			<img src="/images/warren/sejaBestLogosWhite.svg" alt="" srcset="">
-      <CustomButton :text="'Entre em contato'" @click="onButtonClick" />
-      <img src="/images/warren/proDisplay@1x.png" class="absolute -bottom-1/4 z-10" alt="" srcset="">
-		</div>
-	</section>
+  <section
+    class="px-5 flex flex-col gap-6 items-center pt-18 pb-40 mb-36 relative z-0"
+  >
+    <img
+      src="/images/warren/mobile/amanhecer@5x.png"
+      class="absolute -z-10 top-0 h-full object-fill"
+      alt="background"
+      role="presentation"
+    />
+    <img src="/images/warren/logoWarrenWhite.svg" alt="" srcset="" />
+    <h1 class="text-3xl font-bold text-center text-white">
+      Fazer crédito toma tempo, mas não precisa ser o seu
+    </h1>
+    <img src="/images/warren/sejaBestLogosWhite.svg" alt="" srcset="" />
+    <CustomButton :text="'Entre em contato'" @click="onButtonClick" />
+    <img
+      src="/images/warren/proDisplay@1x.png"
+      class="absolute -bottom-1/4 z-10"
+      alt=""
+      srcset=""
+    />
+  </section>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -16,15 +28,15 @@ import CustomButton from '@/components/warren/customButton.component.vue'
 export default defineComponent({
   name: 'HeroSection',
   components: {
-    CustomButton
+    CustomButton,
   },
   data() {
     return {}
   },
   methods: {
-    onButtonClick(){
+    onButtonClick() {
       console.log('Hero Button clicked')
-    }
-  }
+    },
+  },
 })
 </script>
