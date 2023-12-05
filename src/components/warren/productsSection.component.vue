4b6c7764dbe1d7da3404326e40ae635121ddb230
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-12-05 11:20:48 -0300

    update slider controls

--- src/components/warren/productsSection.component.vue
+++ src/components/warren/productsSection.component.vue
@@ -6,30 +6,81 @@
     <p class="text-[#757680]">
       <b>Escolha a opção</b> que melhor se encaixa nas suas <b>necessidades:</b>
     </p>
-    <component :is="'swiper-container'" v-bind="sliderOptions" class="w-full">
+    <div class="relative z-0">
       <component
-        :is="'swiper-slide'"
-        v-for="{ id, description, options, title } of slides"
-        :key="id"
-        class=" bg-white p-5 rounded-2xl border block mb-12"
+        :is="'swiper-container'"
+        id="productSliderWarren"
+        v-bind="sliderOptions"
+        class="w-full"
       >
-        <div class="flex flex-col gap-5 items-start text-start ">
-          <h3 class="text-3xl font-bold text-[#333131]">
-            {{ title }}
-          </h3>
-          <p class="text-[#807C7B] text-xs">
-            {{ description }}
-          </p>
-          <hr class="border border-[#E5E7EB] w-full">
-          <div class="flex flex-col gap-2">
-            <div v-for="option of options" :key="option.id" class="flex flex-row gap-2">
+        <component
+          :is="'swiper-slide'"
+          v-for="{ id, description, options, title } of slides"
+          :key="id"
+          class="bg-white p-5 rounded-2xl border block mb-12"
+        >
+          <div class="flex flex-col gap-5 items-start text-start">
+            <h3 class="text-3xl font-bold text-[#333131]">
+              {{ title }}
+            </h3>
+            <p class="text-[#807C7B] text-xs">
+              {{ description }}
+            </p>
+            <hr class="border border-[#E5E7EB] w-full" />
+            <div class="flex flex-col gap-2">
+              <div
+                v-for="option of options"
+                :key="option.id"
+                class="flex flex-row gap-2"
+              >
                 <i :class="option.icon"></i>
-                <span class="text-[#807C7B] text-xs">{{ option.description }}</span>
+                <span class="text-[#807C7B] text-xs">{{
+                  option.description
+                }}</span>
+              </div>
             </div>
           </div>
-        </div>
+        </component>
       </component>
-    </component>
+      <div
+        class="absolute bottom-2 h-fit flex flex-row justify-between w-full z-[1]"
+      >
+        <button
+          class="wNavigationPrev w-5 h-5 relative text-red-500 fill-blue-600"
+        >
+          <svg
+            width="20"
+            height="20"
+            class="fill-[#757680] hover:fill-red-500 transition-all"
+          >
+            <path
+              d="m9.12 7.259-1.837 1.99H15v1.5H7.282l1.839 1.992-1.103 1.018-2.687-2.911a1.25 1.25 0 0 1 0-1.696l2.687-2.91 1.103 1.017Z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M0 9.938C0 16.149 3.077 20 10.06 20 16.922 20 20 16.15 20 9.938 20 3.602 16.923 0 10.06 0 3.076 0 0 3.602 0 9.938ZM10.06 18.5c-3.221 0-5.284-.882-6.558-2.232C2.213 14.903 1.5 12.816 1.5 9.938c0-2.947.716-4.99 1.988-6.3C4.75 2.335 6.81 1.5 10.059 1.5c3.182 0 5.212.831 6.463 2.133C17.784 4.945 18.5 6.99 18.5 9.938c0 2.878-.713 4.969-1.992 6.335-1.261 1.349-3.295 2.227-6.449 2.227Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+        <button class="wNavigationNext">
+          <svg
+            width="20"
+            height="20"
+            class="fill-[#757680] hover:fill-red-500 transition-all rotate-180"
+          >
+            <path
+              d="m9.12 7.259-1.837 1.99H15v1.5H7.282l1.839 1.992-1.103 1.018-2.687-2.911a1.25 1.25 0 0 1 0-1.696l2.687-2.91 1.103 1.017Z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M0 9.938C0 16.149 3.077 20 10.06 20 16.922 20 20 16.15 20 9.938 20 3.602 16.923 0 10.06 0 3.076 0 0 3.602 0 9.938ZM10.06 18.5c-3.221 0-5.284-.882-6.558-2.232C2.213 14.903 1.5 12.816 1.5 9.938c0-2.947.716-4.99 1.988-6.3C4.75 2.335 6.81 1.5 10.059 1.5c3.182 0 5.212.831 6.463 2.133C17.784 4.945 18.5 6.99 18.5 9.938c0 2.878-.713 4.969-1.992 6.335-1.261 1.349-3.295 2.227-6.449 2.227Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
   </section>
 </template>
 <script lang="ts">
@@ -42,8 +93,18 @@ export default defineComponent({
     const sliderOptions: SwiperOptions = {
       spaceBetween: 20,
       slidesPerView: 'auto',
-      pagination: true,
-      autoHeight: true,
+      pagination: {
+        clickable: true,
+        renderBullet: (index, className) => {
+          return `<span class="${className}"></span>`
+        },
+      },
+      navigation: {
+        enabled: true,
+        prevEl: '.wNavigationPrev',
+        nextEl: '.wNavigationNext',
+      },
+      // autoHeight: true,
       breakpoints: {
         1280: {
           slidesPerView: 4,
@@ -106,8 +167,7 @@ export default defineComponent({
       {
         id: 3,
         title: 'Crédito com Veículo de garantia',
-        description:
-          'Use seu carro, caminhão ou utilitário como garantia.',
+        description: 'Use seu carro, caminhão ou utilitário como garantia.',
         options: [
           {
             id: 1,
@@ -156,3 +216,20 @@ export default defineComponent({
   },
 })
 </script>
+
+<style lang="scss" scoped>
+swiper-container {
+  &::part(pagination) {
+    @apply absolute inset-x-0 flex flex-row items-center gap-5 w-auto justify-center;
+  }
+
+  &::part(bullet) {
+    @apply inline-block rounded-full w-3 h-3 transition-all;
+    background: #d9d9d9;
+  }
+
+  &::part(bullet-active) {
+    @apply inline-block bg-red-500 w-5 h-5 rounded-full transition-all;
+  }
+}
+</style>
