2d7bb4ac1bde1eb527eb3bda64c754265fa51dce
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-12-05 17:55:18 -0300

    update hero section

--- src/components/warren/heroSection.component.vue
+++ src/components/warren/heroSection.component.vue
@@ -3,22 +3,27 @@
     class="px-5 flex flex-col gap-6 items-center pt-18 pb-40 mb-36 relative z-0"
   >
     <img
-      src="/images/warren/mobile/amanhecer@5x.png"
+      src="/images/warren/mobile/amanhecer.png"
       class="absolute -z-10 top-0 w-full h-full object-fill"
       alt="background"
       role="presentation"
     />
     <LogoWarrenSVG class="flex mx-auto h-auto w-44 text-white" />
-    <h1 class="text-3xl font-bold text-center text-white">
+    <h1 class="text-2xl font-bold text-center text-white">
       Fazer crédito toma tempo, mas não precisa ser o seu
     </h1>
     <img src="/images/warren/sejaBestLogosWhite.svg" alt="" srcset="" />
-    <CustomButton :text="'Entre em contato'" @click="onButtonClick" />
+    <CustomButton :text="'Quero simular meu crédito'" @click="onButtonClick" />
     <img
       src="/images/warren/proDisplay@1x.png"
-      class="absolute -bottom-1/4 z-10"
+      srcset="
+        /images/warren/proDisplay@1x.png 1x,
+        /images/warren/proDisplay@2x.png 2x,
+        /images/warren/proDisplay@4x.png 4x,
+      "
+      sizes="(max-width: 600px) 200px, 400px"
+      class="absolute -bottom-1/4 z-10 h-64"
       alt=""
-      srcset=""
     />
   </section>
 </template>
