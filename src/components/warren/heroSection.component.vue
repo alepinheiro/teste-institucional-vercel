e89d9f3614da50f8b11fc3ada2a08273c878bac7
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-12-06 14:05:23 -0300

    update desktop styles hero section

--- src/components/warren/heroSection.component.vue
+++ src/components/warren/heroSection.component.vue
@@ -1,29 +1,39 @@
 <template>
   <section
-    class="px-5 flex flex-col gap-6 items-center pt-18 pb-40 mb-36 relative z-0"
+    class="relative z-0 min-h-screen h-screen flex flex-col items-stretch overflow-hidden"
   >
-    <img
-      src="/images/warren/mobile/amanhecer.png"
-      class="absolute -z-10 top-0 w-full h-full object-fill"
-      alt="background"
-    />
-    <LogoWarrenSVG class="flex mx-auto h-auto w-44 text-white" />
-    <h1 class="text-2xl font-bold text-center text-white">
-      Fazer crédito toma tempo, mas não precisa ser o seu
-    </h1>
-    <img src="/images/warren/sejaBestLogosWhite.svg" alt="" srcset="" />
-    <CustomButton :text="'Quero simular meu crédito'" @click="onButtonClick" />
-    <img
-      src="/images/warren/proDisplay@1x.png"
-      srcset="
-        /images/warren/proDisplay@1x.png 1x,
-        /images/warren/proDisplay@2x.png 2x,
-        /images/warren/proDisplay@4x.png 4x,
-      "
-      sizes="(max-width: 600px) 200px, 400px"
-      class="absolute -bottom-1/4 z-10 h-64"
-      alt=""
-    />
+    <div
+      class="h-4/5 flex flex-grow bg-[url(/images/warren/desktop/amanhecer.png)] bg-cover px-5"
+    >
+      <div class="md:max-w-3xl lg:max-w-5xl xl:max-w-6xl m-auto w-full">
+        <div class="flex flex-col gap-6 items-center md:items-start lg:items-start xl:items-start md:w-5/6 lg:w-4/6 xl:w-1/2">
+          <LogoWarrenSVG class="flex h-auto w-44 text-white" />
+          <h1
+            class="text-2xl md:text-5xl lg:text-5xl font-bold text-center md:text-start lg:text-start xl:text-start text-white"
+          >
+            Fazer crédito toma tempo, mas não precisa ser o seu
+          </h1>
+          <img src="/images/warren/sejaBestLogosWhite.svg" alt="" srcset="" />
+          <CustomButton
+            :text="'Quero simular meu crédito'"
+            @click="onButtonClick"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="h-1/5 flex w-full xl:h-0">
+      <img
+        src="/images/warren/proDisplay@1x.png"
+        srcset="
+          /images/warren/proDisplay@1x.png 1x,
+          /images/warren/proDisplay@2x.png 2x,
+          /images/warren/proDisplay@4x.png 4x
+        "
+        sizes="(max-width: 600px) 200px, 400px"
+        class="h-64 md:h-96 mx-auto inset-x-0 absolute md:left-auto lg:left-auto bottom-0 lg:h-4/5 lg:-inset-x-1/4 xl:top-1/4 xl:left-auto xl:right-12 xl:h-2/3"
+        alt=""
+      />
+    </div>
   </section>
 </template>
 <script lang="ts">
