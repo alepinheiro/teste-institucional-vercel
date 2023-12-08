2a83c3af87d8ab0fac63a4775157ccee143aa18d
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-12-08 09:55:10 -0300

    update hero section

--- src/components/warren/heroSection.component.vue
+++ src/components/warren/heroSection.component.vue
@@ -6,10 +6,10 @@
       class="h-4/5 flex flex-grow bg-[url(/images/warren/desktop/amanhecer.png)] bg-cover px-5"
     >
       <div
-        class="md:max-w-3xl lg:max-w-5xl xl:max-w-6xl m-auto w-full xl:flex xl:flex-row gap-5"
+        class="md:max-w-2xl lg:max-w-5xl xl:max-w-6xl m-auto w-full xl:flex xl:flex-row gap-5"
       >
         <div
-          class="xl:h-134 flex flex-col gap-6 items-center md:items-start lg:items-start xl:items-start xl:justify-between md:w-5/6 lg:w-4/6 xl:w-1/2 xl:flex-1"
+          class="xl:h-134 flex flex-col gap-5 md:gap-8 lg:gap-8 xl:gap-8  items-center md:items-start lg:items-start xl:items-start xl:justify-between md:w-5/6 lg:w-4/6 xl:w-1/2 xl:flex-1"
         >
           <LogoWarrenSVG class="flex h-auto w-44 text-white" />
           <h1
@@ -34,14 +34,13 @@
     </div>
     <div class="h-1/5 flex w-full xl:h-0 xl:hidden">
       <img
-        src="/images/warren/proDisplay@1x.png"
+        src="/images/warren/proDisplay@2x.png"
         srcset="
           /images/warren/proDisplay@1x.png 1x,
           /images/warren/proDisplay@2x.png 2x,
           /images/warren/proDisplay@4x.png 4x
         "
-        sizes="(max-width: 600px) 200px, 400px"
-        class="h-80 md:h-96 mx-auto inset-x-0 absolute md:left-auto lg:left-auto bottom-0 lg:h-4/5 lg:-inset-x-1/4 xl:top-1/4 xl:left-auto xl:right-12 xl:h-2/3"
+        class="h-80 md:h-96 md:scale-110 mx-auto inset-x-0 absolute md:left-auto lg:left-[63%] bottom-0 lg:h-5/6 xl:top-1/4 xl:left-auto xl:right-12 xl:h-2/3 scale-95"
         alt=""
       />
     </div>
