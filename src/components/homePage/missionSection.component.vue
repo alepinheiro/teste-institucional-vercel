72c27476c4c87e74ea8cd5da12547353769f6d93
Ale Pinheiro <alessandropsbra@gmail.com>
2023-12-11 14:34:23 -0300

    update mission section

--- src/components/homePage/missionSection.component.vue
+++ src/components/homePage/missionSection.component.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="py-10 px-5 flex flex-col gap-8">
+  <section class="py-10 px-5 flex flex-col gap-8 md:max-w-2xl mx-auto">
     <div class="text-center flex flex-col gap-4">
       <div class="flex flex-col gap-2">
-        <h2 class="text-primary text-2xl font-bold">Por que existimos?</h2>
-        <p>
+        <h2 class="text-primary text-2xl md:text-3xl font-bold">Por que existimos?</h2>
+        <p class="md:text-xl">
           Acreditamos que o acesso ao crédito pode levar as pessoas a sua melhor
           versão.
         </p>
@@ -15,20 +15,20 @@
       </p>
     </div>
 
-    <div class="flex flex-col rounded-xl overflow-hidden border">
-      <div class="flex flex-col font-bold">
+    <div class="flex flex-col md:flex-row rounded-xl overflow-hidden border">
+      <div class="flex flex-col font-bold md:flex-1">
         <button
           v-for="{ id, title, isActive, image } of options"
           :key="id"
           :class="{ 'bg-primary text-white': isActive }"
-          class="border-b p-4 text-textPrimary w-full text-start"
+          class="border-b p-4 md:py-11 text-textPrimary w-full text-start "
           @click="changeActiveImage(image)"
         >
           {{ id }}. {{ title }}
         </button>
       </div>
-      <div class="h-48 bg-primary">
-        <img :src="activeImage" alt="" />
+      <div class="h-48 bg-primary md:flex-1 md:h-auto">
+        <img :src="activeImage" alt="" class="object-cover h-full" />
       </div>
     </div>
   </section>
@@ -39,14 +39,14 @@ export default defineComponent({
   name: 'MissionSection',
   data() {
     return {
-      activeImage: 'https://dummyimage.com/600x600.png?text=default',
+      activeImage: 'https://dummyimage.com/1000x1000.png?text=default',
       options: [
         {
           id: 1,
           title: 'Educação',
           description:
             'Através de conteúdos gratuitos que produzimos para empoderar o cliente na tomada da melhor decisão. Confira clicando abaixo:',
-          image: 'https://dummyimage.com/600x600.png?text=01',
+          image: 'https://dummyimage.com/1000x1000.png?text=01',
           isActive: true,
         },
         {
@@ -54,7 +54,7 @@ export default defineComponent({
           title: 'Tecnologia',
           description:
             'Através de conteúdos gratuitos que produzimos para empoderar o cliente na tomada da melhor decisão. Confira clicando abaixo:',
-          image: 'https://dummyimage.com/600x600.png?text=02',
+          image: 'https://dummyimage.com/1000x1000.png?text=02',
           isActive: false,
         },
         {
@@ -62,7 +62,7 @@ export default defineComponent({
           title: 'Experiência Humanizada',
           description:
             'Através de conteúdos gratuitos que produzimos para empoderar o cliente na tomada da melhor decisão. Confira clicando abaixo:',
-          image: 'https://dummyimage.com/600x600.png?text=03',
+          image: 'https://dummyimage.com/1000x1000.png?text=03',
           isActive: false,
         },
       ],
