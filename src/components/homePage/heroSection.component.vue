8626310a6e74a063cb3f9a6ff66ec9edc470c44f
Ale Pinheiro <alessandropsbra@gmail.com>
2023-12-11 14:58:54 -0300

    update hero section

--- src/components/homePage/heroSection.component.vue
+++ src/components/homePage/heroSection.component.vue
@@ -16,14 +16,14 @@
       class="flex flex-col gap-4 items-center mt-auto mx-auto h-full px-5 pb-12"
     >
       <div class="flex flex-col gap-4 text-center text-white font-bold">
-        <h1 class="text-3xl md:text-5xl font-darkerGrotesque tracking-wider">
+        <h1 class="text-3xl md:text-5xl font-darkerGrotesque tracking-wide font-[700]">
           A melhor fintech de Crédito do Brasil
         </h1>
         <h2 class="text-xl md:text-2xl">Sem filas, Sem gerentes, Sem complexidade.</h2>
       </div>
       <a
         :href="information.appSimulator + $root.utms"
-        class="bg-complementaryColor1 text-white font-bold w-fit px-5 py-3 rounded-md"
+        class="bg-complementaryColor1 text-white font-bold w-fit px-5 py-3 rounded-md md:text-xl"
       >
         Simule seu crédito
       </a>
