172690a604e02dd8b2bfb6cb45e60dd72f20026a
Ale Pinheiro <alessandropsbra@gmail.com>
2023-12-11 14:14:49 -0300

    update about us section

--- src/components/homePage/aboutUs.component.vue
+++ src/components/homePage/aboutUs.component.vue
@@ -1,33 +1,47 @@
 <template>
-  <section class="bg-[#DEDEDE] px-5 py-10">
-    <div class="flex flex-col gap-2 text-center">
-      <p class="text-textSecondary">
-        A <b> SejaBest </b> já tem história no mercado.
-      </p>
-      <h2 class="text-xl">
-        <strong>Há 10 anos</strong> transformamos o acesso ao crédito
-      </h2>
-    </div>
+  <section class="bg-[#DEDEDE]">
+    <div class="px-5 py-10 md:max-w-2xl mx-auto">
+      <div class="flex flex-col gap-2 text-center">
+        <p class="text-textSecondary md:text-textPrimary">
+          A <b> SejaBest </b> já tem história no mercado.
+        </p>
+        <h2 class="text-xl">
+          <strong>Há 10 anos</strong> transformamos o acesso ao crédito
+        </h2>
+      </div>
 
-    <ul class="flex flex-row gap-5 py-10">
-      <li v-for="{ icon, title, description } in cards" :key="icon" class="p-5 bg-white drop-shadow-lg rounded w-full">
-        <i :class="icon" class="text-textSecondary block"></i>
-        <span class="font-bold text-xl text-primary block">
-          {{ title }}
-        </span>
-        <span class="text-textSecondary block">
-          {{ description }}
-        </span>
-      </li>
-    </ul>
+      <ul class="flex flex-row gap-5 py-10">
+        <li
+          v-for="{ icon, title, description } in cards"
+          :key="icon"
+          class="p-5 md:py-10 bg-white drop-shadow-lg rounded w-full flex flex-col md:gap-6"
+        >
+        <div class="md:h-7 md:w-7">
+          <i :class="icon" class="text-textSecondary block w-full h-full"></i>
+        </div>
+          <span class="font-bold text-xl md:text-5xl text-primary block">
+            {{ title }}
+          </span>
+          <span class="text-textSecondary block md:text-xl">
+            {{ description }}
+          </span>
+        </li>
+      </ul>
 
-    <div class="sm:flex flex-row flex-wrap items-center gap-6 mx-auto hidden">
       <div
-        v-for="{ alt, id, source } of seals"
-        :key="id"
-        class="w-1/3 last-of-type:w-1/2 last-of-type:mx-auto last-of-type:flex-grow-0 flex-grow"
+        class="sm:flex md:hidden flex-row flex-wrap items-center gap-6 mx-auto"
       >
-        <img :src="`/images/Home/seals/dark/${source}`" :alt="alt" class="h-24 sm:h-auto" />
+        <div
+          v-for="{ alt, id, source } of seals"
+          :key="id"
+          class="w-1/3 last-of-type:w-1/2 last-of-type:mx-auto last-of-type:flex-grow-0 flex-grow"
+        >
+          <img
+            :src="`/images/Home/seals/dark/${source}`"
+            :alt="alt"
+            class="h-24 sm:h-auto"
+          />
+        </div>
       </div>
     </div>
   </section>
