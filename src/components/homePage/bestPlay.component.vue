c0058fb12e5bd7fd0552d9555f12d234b193cbfb
Ale Pinheiro <alessandropsbra@gmail.com>
2023-12-11 14:52:36 -0300

    update best play

--- src/components/homePage/bestPlay.component.vue
+++ src/components/homePage/bestPlay.component.vue
@@ -1,29 +1,48 @@
 <template>
-  <section class="flex flex-col pt-10">
-    <div class="px-5">
+  <section
+    class="flex flex-col pt-10 md:pt-0 md:mt-10 md:bg-[#181818] relative"
+  >
+    <div class="px-5 md:hidden">
       <img
-        src="/images/Home/bestPlayMockup.png"
+        src="/images/Home/bestPlayMockupMobile.png"
         alt="Conheça o maior canal de crédito do Brasil"
         loading="lazy"
         class="mx-auto"
       />
     </div>
-    <div
-      class="bg-[#181818] flex flex-col gap-5 items-center px-5 py-10 text-white text-center"
-    >
-      <BestPlayLogo />
-      <h2 class="text-2xl font-bold">
-        Conheça o maior canal de crédito do Brasil
-      </h2>
-      <p class="text-justify">
-        BestPlay é o canal de comunicação da SejaBest no YouTube. Com mais de
-        500 vídeos, nosso canal reúne uma comunidade de mais de 50 mil pessoas.
-        Abordamos temas como crédito, financiamento, empreendedorismo e mercado
-        financeiro, com o único propósito de levar você à sua melhor versão.
-      </p>
-      <a href="https://www.youtube.com/@SejaBest" class="bg-complementaryColor2 w-fit rounded px-6 py-4"
-        >Inscreva-se agora</a
+    <div class="md:max-w-2xl mx-auto z-0">
+      <div
+        class="bg-[#181818] flex flex-col gap-5 items-center px-5 py-10 text-white text-center md:text-left md:w-4/6"
       >
+        <BestPlayLogo class="md:hidden" />
+        <h2 class="text-2xl md:text-3xl font-bold">
+          Conheça o maior canal de crédito do Brasil
+        </h2>
+        <p class="text-justify md:leading-9">
+          BestPlay é o canal de comunicação da SejaBest no YouTube. Com mais de
+          500 vídeos, nosso canal reúne uma comunidade de mais de 50 mil
+          pessoas. Abordamos temas como crédito, financiamento, empreendedorismo
+          e mercado financeiro, com o único propósito de levar você à sua melhor
+          versão.
+        </p>
+        <div class="flex flex-row justify-between w-full">
+          <a
+            href="https://www.youtube.com/@SejaBest"
+            class="bg-complementaryColor2 w-fit rounded px-6 py-4"
+          >
+            Inscreva-se agora
+          </a>
+          <BestPlayLogo class="hidden md:block" />
+        </div>
+        <div class="hidden md:flex md:absolute right-0 bottom-0 h-[110%] z-0">
+          <img
+            src="/images/Home/bestPlayMockupDesktop.png"
+            alt="Conheça o maior canal de crédito do Brasil"
+            loading="lazy"
+            class="mx-auto"
+          />
+        </div>
+      </div>
     </div>
   </section>
 </template>
--- src/views/TheIndex.vue
+++ src/views/TheIndex.vue
@@ -24,16 +24,16 @@
   <TheFooter />
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HeroSection from '@/components/homePage/heroSection.component.vue'
-import Products from '@/components/homePage/productsSection.component.vue'
-import BusinessPartners from '@/components/homePage/businessPartners.component.vue'
-import SloganSection from '@/components/homePage/sloganSection.component.vue'
 import AboutUs from '@/components/homePage/aboutUs.component.vue'
-import Mission from '@/components/homePage/missionSection.component.vue'
-import ComparisonTable from '@/components/base/comparisonTable.component.vue'
 import BestPlay from '@/components/homePage/bestPlay.component.vue'
+import BusinessPartners from '@/components/homePage/businessPartners.component.vue'
+import ComparisonTable from '@/components/base/comparisonTable.component.vue'
+import HeroSection from '@/components/homePage/heroSection.component.vue'
+import Mission from '@/components/homePage/missionSection.component.vue'
+import Products from '@/components/homePage/productsSection.component.vue'
+import SloganSection from '@/components/homePage/sloganSection.component.vue'
 import TheFooter from '@/components/structure/theFooter.component.vue'
+import { defineComponent } from 'vue'
 import { useHead } from '@vueuse/head'
 
 export default defineComponent({
