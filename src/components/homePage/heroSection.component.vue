ffdb695a9e399ed38f6fb4cb721c689ad844a284
Ale Pinheiro <alessandropsbra@gmail.com>
2023-12-11 10:05:51 -0300

    update hero slider

--- src/components/homePage/heroSection.component.vue
+++ src/components/homePage/heroSection.component.vue
@@ -1,33 +1,47 @@
 <template>
   <section class="bg-bgDarkColor min-h-screen relative z-0 flex">
-
+    <div class="absolute inset-x-0 h-fit top-0 flex justify-center z-20">
+      <Topbar class=" w-full max-w-7xl lg:max-w-5xl px-5 mb-auto pt-4" />
+    </div>
     <picture class="absolute top-0 inset-x-0 bottom-0 -z-10">
-      <source type="image/webp">
+      <source type="image/webp" />
       <img
-      src="/images/Home/heroBackground.png"
-      alt=""
-      srcset=""
-      class="object-cover h-full w-full">
+        src="/images/Home/heroBackground.png"
+        alt="A melhor fintech de Crédito do Brasil"
+        class="object-cover h-full w-full"
+      />
     </picture>
 
-    <div class=" flex flex-col gap-4 items-center mt-auto mx-auto h-full px-5 pb-12">
+    <div
+      class="flex flex-col gap-4 items-center mt-auto mx-auto h-full px-5 pb-12"
+    >
       <div class="flex flex-col gap-4 text-center text-white font-bold">
-        <h1 class="text-3xl font-darkerGrotesque ">A melhor fintech de Crédito do Brasil</h1>
+        <h1 class="text-3xl font-darkerGrotesque tracking-wider">
+          A melhor fintech de Crédito do Brasil
+        </h1>
         <h2 class="text-xl">Sem filas, Sem gerentes, Sem complexidade.</h2>
       </div>
-      <a href="#" class="bg-complementaryColor1 text-white font-bold w-fit px-5 py-3 rounded-md">
+      <a
+        :href="information.appSimulator + $root.utms"
+        class="bg-complementaryColor1 text-white font-bold w-fit px-5 py-3 rounded-md"
+      >
         Simule seu crédito
       </a>
     </div>
-
   </section>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Topbar from '@/components/structure/topbar.component.vue'
+import information from '@/configurations/information';
+
 export default defineComponent({
   name: 'HeroSection',
+  components: { Topbar },
   data() {
-    return {}
+    return {
+      information
+    }
   },
 })
 </script>
