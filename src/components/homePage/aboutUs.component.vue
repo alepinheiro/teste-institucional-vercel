495b8126eefc313dee43dee3729dce7dab3ffedf
Ale Pinheiro <alessandropsbra@gmail.com>
2023-12-11 16:12:25 -0300

    update aboutUs section

--- src/components/homePage/aboutUs.component.vue
+++ src/components/homePage/aboutUs.component.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="bg-[#DEDEDE] md:bg-white relative z-0 md:mt-10">
+  <section class="bg-[#DEDEDE] md:bg-white lg:bg-white xl:bg-white relative z-0 md:mt-10 lg:mt-12 xl:mt-12">
     <div class="bg-gradient-to-b from-[#DEDEDE] to-white absolute top-0 bottom-1/3 inset-x-20 -z-10 rounded-xl"></div>
-    <div class="px-5 py-10 md:max-w-2xl mx-auto">
-      <div class="flex flex-col gap-2 text-center md:text-left">
+    <div class="px-5 lg:px-0 py-10 md:max-w-2xl lg:max-w-5xl mx-auto flex flex-col lg:flex-row items-center">
+      <div class="flex flex-col gap-2 text-center md:text-left lg:text-left xl:text-left lg:w-5/12">
         <p class="text-textSecondary md:text-textPrimary ">
           A <b> SejaBest </b> já tem história no mercado.
         </p>
-        <h2 class="text-xl md:text-4xl md:text-textPrimary">
+        <h2 class="text-xl md:text-4xl lg:text-5xl xl:text-5xl md:text-textPrimary lg:text-textPrimary xl:text-textPrimary">
           <strong>
             Há 10 anos
           </strong>
@@ -14,16 +14,16 @@
         </h2>
       </div>
 
-      <ul class="flex flex-row gap-5 py-10">
+      <ul class="flex flex-row gap-5 py-10 lg:w-7/12">
         <li
           v-for="{ icon, title, description } in cards"
           :key="icon"
-          class="p-5 md:py-10 bg-white drop-shadow-lg md:shadow-2xl rounded md:rounded-xl w-full flex flex-col md:gap-6"
+          class="p-5 md:py-10 lg:py-10 bg-white drop-shadow-lg md:shadow-2xl rounded md:rounded-xl w-full flex flex-col md:gap-6 lg:gap-3 lg:h-full"
         >
-        <div class="md:h-7 md:w-7">
+        <div class="md:h-7 md:w-7 lg:w-7 lg:h-7 xl:w-7 xl:h-7">
           <i :class="icon" class="text-textSecondary block w-full h-full"></i>
         </div>
-          <span class="font-bold text-xl md:text-5xl text-primary block">
+          <span class="font-bold text-xl md:text-5xl lg:text-5xl xl:text-5xl text-primary block">
             {{ title }}
           </span>
           <span class="text-textSecondary block md:text-xl">
@@ -33,7 +33,7 @@
       </ul>
 
       <div
-        class="sm:flex md:hidden flex-row flex-wrap items-center gap-6 mx-auto"
+        class="flex md:hidden lg:hidden xl:hidden flex-row flex-wrap items-center gap-6 mx-auto"
       >
         <div
           v-for="{ alt, id, source } of seals"
