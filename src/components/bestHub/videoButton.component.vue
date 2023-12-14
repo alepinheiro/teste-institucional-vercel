f52cfb21b792fe082ccea22466881036eaac5933
Ale Pinheiro <alessandropsbra@gmail.com>
2023-12-13 21:03:02 -0300

    update video button component

--- src/components/bestHub/videoButton.component.vue
+++ src/components/bestHub/videoButton.component.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="relative w-16 mx-auto my-10 transition-all z-50">
-
-    <PlayVideo class="absolute h-16 top-0 -z-10 inset-x-0 " @click="handleOverlay" />
-
+    <Overlay
+      v-if="showVideo"
+      :video-id="videoId"
+      @closeOverlay="showVideo = false"
+    />
+    <PlayButton
+      class="absolute h-16 top-0 -z-10 inset-x-0"
+      @click="showVideo = true"
+    />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-import PlayVideo from '@/components/base/buttons/playVideo.component.vue'
+import PlayButton from '@/components/base/buttons/playVideo.component.vue'
 import Overlay from '@/components/base/overlays/youtubeVideo.component.vue'
 
 export default defineComponent({
   name: 'VideoButton',
-  components: { PlayVideo },
+  components: { PlayButton, Overlay },
   props: {
     videoId: {
       type: String,
@@ -24,33 +30,5 @@ export default defineComponent({
       showVideo: false,
     }
   },
-  methods: {
-    handleOverlay() {
-      this.showVideo = !this.showVideo
-    },
-  },
 })
 </script>
-<style lang="scss">
-.overlay {
-  &::after {
-    @apply absolute inset-0 bg-black/60 z-10 blur-lg;
-    content: ' ';
-  }
-}
-
-.bh-videoPlayer {
-  @apply aspect-video;
-
-  @screen sm {
-    width: 80vw;
-  }
-
-  @screen md {
-    width: 75vw;
-  }
-
-  width: 50vw;
-  height: auto;
-}
-</style>
