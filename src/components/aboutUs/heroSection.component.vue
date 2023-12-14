32eb0788965fba242cf8b775edd12794ce574997
Alessandro Pinheiro <alessandropsbra@gmail.com>
2023-12-14 09:52:25 -0300

    update hero section css

--- src/components/aboutUs/heroSection.component.vue
+++ src/components/aboutUs/heroSection.component.vue
@@ -1,9 +1,14 @@
 <template>
-  <section class="flex h-screen">
+  <section class="flex relative h-screen z-0">
     <div class="absolute inset-x-0 h-fit top-0 flex justify-center z-20">
       <TopBar class="w-full max-w-7xl lg:max-w-5xl px-5 mb-auto pt-4" />
     </div>
-    <picture class="absolute top-0 inset-x-0 bottom-[75%] -z-10">
+
+    <div
+      class="bg-primary rounded-full h-44 absolute top-0 inset-x-0 w-full bottom-1/4 -z-20 blur-2xl mt-auto"
+    ></div>
+
+    <picture class="absolute top-0 inset-x-0 bottom-1/4 -z-10">
       <source type="image/png" />
       <img
         src="/images/Home/heroBackground.png"
@@ -11,9 +16,14 @@
         class="object-cover h-full w-full"
       />
     </picture>
-    <div class="text-white text-2xl mt-auto pb-12 font-bold px-5 text-center">
-      <h2> Temos uma missão grandiosa: </h2>
-      <h1 class="font-darkerGrotesque text-4xl"> “Transformar o acesso ao crédito para quem busca sua melhor versão” </h1>
+
+    <div
+      class="text-white text-2xl mt-auto pb-12 font-bold px-5 text-center h-3/5 flex flex-col justify-center"
+    >
+      <h2>Temos uma missão grandiosa:</h2>
+      <h1 class="font-darkerGrotesque text-4xl">
+        “Transformar o acesso ao crédito para quem busca sua melhor versão”
+      </h1>
     </div>
   </section>
 </template>
