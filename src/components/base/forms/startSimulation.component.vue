3968aafe3313515426956cd4106c963ee7f59ef1
BruEloiseHinrichs <bruna.hinrichs@seja.best>
2024-01-08 09:00:12 -0300

    ajuste placehonder valor home

--- src/components/base/forms/startSimulation.component.vue
+++ src/components/base/forms/startSimulation.component.vue
@@ -35,7 +35,6 @@
           ref="realtyValue"
           name="realtyValue"
           type="text"
-          placeholder="R$ 250.000,00"
           :required="showHomeEquity"
           class="bg-zinc-200 rounded p-2 focus-within:shadow-md transition-all ring-0 focus-visible:ring-4 focus-visible:ring-blue-600 focus-visible:outline-none"
         />
