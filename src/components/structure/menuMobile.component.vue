31052e203c09a003ce2ff01b140b04b8940d91f5
Ale Pinheiro <alessandropsbra@gmail.com>
2024-02-05 21:26:04 -0300

    update menu icon

--- src/components/structure/menuMobile.component.vue
+++ src/components/structure/menuMobile.component.vue
@@ -1,22 +1,13 @@
 <template>
-  <nav class="flex mb-auto w-fit">
+  <nav class="flex">
     <button
       id="divMenu"
       title="Menu"
       aria-label="menu"
-      class="cursor-pointer w-fit"
+      class="cursor-pointer w-8 h-8 flex"
       @click="toggleMenu(), handleClickOutside($event)"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-full w-8"
-        fill="currentColor"
-        viewBox="0 0 448 512"
-      >
-        <path
-          d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z"
-        />
-      </svg>
+      <i class="fa-solid fa-bars w-full h-full"></i>
     </button>
     <div
       v-if="openProfileMenu"
