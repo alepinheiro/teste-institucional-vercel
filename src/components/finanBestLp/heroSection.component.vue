554f708981021744ea8f7a31b7cfcf738e6e6a47
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-02-08 13:51:07 -0300

    remove modal

--- src/components/finanBestLp/heroSection.component.vue
+++ src/components/finanBestLp/heroSection.component.vue
@@ -16,9 +16,7 @@
       class="relative max-w-7xl lg:max-w-5xl md:max-w-2xl mx-auto flex flex-row w-full z-10 py-0 min-md:py-12 px-0 min-md:px-5"
     >
       <!--  -->
-      <div
-        class="lg:w-7/12 xl:w-5/12 lg:flex-initial mt-auto mb-12 md:mb-0 "
-      >
+      <div class="lg:w-7/12 xl:w-5/12 lg:flex-initial mt-auto mb-12 md:mb-0">
         <div
           class="bg-gradient-to-b min-md:bg-gradient-to-t via-white from-transparent to-white/80 flex flex-col gap-6 p-8 xl:px-6 py-4 min-md:rounded-xl min-hd:shadow-lg h-full justify-end"
         >
@@ -50,15 +48,13 @@
               class="flex flex-row gap-2 items-center mx-auto w-full"
             >
               <component :is="icon" class="text-primary flex-shrink-0" />
-              <p
-                class="text-textPrimary text-left text-base"
-              >
+              <p class="text-textPrimary text-left text-base">
                 {{ description }}
               </p>
             </div>
           </div>
 
-          <hr class="border border-textSecondary hidden min-lg:block">
+          <hr class="border border-textSecondary hidden min-lg:block" />
 
           <InputWithSlider
             v-model="formValue"
