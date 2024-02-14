c0b096d59a28eb3fec63559ae5ecc6a3b796a883
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-02-14 18:51:21 -0300

    update video section

--- src/components/finanBestLp/videoSection.component.vue
+++ src/components/finanBestLp/videoSection.component.vue
@@ -6,19 +6,14 @@
       <div
         class="w-5/12 lg:w-7/12 md:w-full sm:w-full flex flex-col gap-8 text-textPrimary text-center"
       >
-        <h2 class="text-4xl sm:text-2xl text-primary font-bold">
-          O FinanBest é o produto de financiamento imobiliário da SejaBest.
+        <h2 class="text-3xl sm:text-2xl font-bold">
+          O <span class="text-primary">FinanBest</span> é o financiamento imobiliário da SejaBest.
         </h2>
         <div class="text-base leading-relaxed sm:hidden">
           <p>
-            Imagine financiar seu novo imóvel sem sair de casa, contando com um
-            time de especialistas prontos para lhe atender a qualquer momento.
-          </p>
-          <br />
-          <p>
+            Financie seu novo imóvel sem sair de casa! Com um time de especialistas sempre disponíveis.
             <b>
-              Assista o vídeo que preparamos especialmente para você e entenda
-              cada detalhe desta operação.
+              Assista ao vídeo exclusivo para entender cada detalhe desta operação.
             </b>
           </p>
         </div>
