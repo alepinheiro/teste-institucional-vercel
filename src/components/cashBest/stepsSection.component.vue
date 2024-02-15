c017c57859ca703b9173c5ff54d37f89f9e1be39
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-02-15 11:14:12 -0300

    update steps

--- src/components/cashBest/stepsSection.component.vue
+++ src/components/cashBest/stepsSection.component.vue
@@ -5,7 +5,7 @@
         class="flex flex-row w-full justify-between items-center md:max-w-2xl md:mx-auto"
       >
         <div class="flex flex-col gap-1">
-          <h2 class="text-4xl sm:text-2xl text-primary font-bold">
+          <h2 class="min-md:text-3xl text-2xl text-primary font-bold">
             Crédito sem filas, nem gerentes, nem complexidade
           </h2>
           <p class="text-base text-textPrimary">
