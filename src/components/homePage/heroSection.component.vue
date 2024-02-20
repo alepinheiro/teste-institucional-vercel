511d67c0ddcb3f157ffab7e2c2d1f3d4acc8bd9e
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-02-20 11:09:35 -0300

    update finanbest padding top

--- src/components/homePage/heroSection.component.vue
+++ src/components/homePage/heroSection.component.vue
@@ -19,7 +19,7 @@
         alt=""
       />
       <div
-        class="z-0 flex flex-col gap-5 min-md:gap-10 items-center min-lg:w-8/12 min-lg:mr-auto xl:items-start lg:items-start text-center min-lg:text-left -mt-24 min-md:mt-0 px-5"
+        class="z-0 flex flex-col gap-5 min-md:gap-10 items-center min-lg:w-8/12 min-lg:mr-auto xl:items-start lg:items-start text-center min-lg:text-left -mt-24 min-md:mt-0 px-5 min-md:pt-20"
       >
         <h1
           class="text-3xl xl:text-6xl lg:text-6xl md:text-6xl font-bold font-darkerGrotesque text-textPrimary leading-7 lg:leading-[48px]"
