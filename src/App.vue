e68aed80704eebda06de6018c3dcf386c0df7f79
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-02-26 14:10:16 -0300

    update get out rent url queries

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 import notificationPopUp from '@/components/base/notificationPopUp.component.vue'
 import { NotificationInterface } from '@/interfaces/notification.interface'
 import { defineComponent } from 'vue'
+import { useObjectToQueryString } from '@/composables/useObjectToQueryString';
 
 export default defineComponent({
   components: { notificationPopUp },
@@ -22,21 +23,8 @@ export default defineComponent({
         showMessage: false,
         buttonText: 'Continuar',
       } as NotificationInterface,
-      // utms: '' as string,
+      utms: useObjectToQueryString(this.$route.query),
     }
-  },
-  mounted() {
-    // let fullUrl = this.$route.fullPath.split('?')
-    // console.log(this.$route.fullPath)
-    // console.log(this.$route.fullPath.includes('?'))
-    // console.log(!!this.$root)
-    // console.log(this.$route.fullPath.includes('?') && this.$root)
-    // if (this.$route.fullPath.includes('?') && this.$root) {
-    //   this.$root.utms = '?' + fullUrl[1]
-    // }
-    // setTimeout(() => {
-    //   this.isLoading = true
-    // }, 600)
   },
 })
 </script>
