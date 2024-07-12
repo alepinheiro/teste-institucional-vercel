895e3d6e742a8d353f176860f3620434f63d45e4
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-07-12 10:22:10 -0300

    update bex play

--- src/components/vehicle/BextCarLP/section2.vue
+++ src/components/vehicle/BextCarLP/section2.vue
@@ -2,42 +2,47 @@
   <section>
     <div class="maxWidth flex flex-wrap pt-0">
       <div class="w-1/2 mt-auto mb-auto sm:w-full sm:hidden">
-        <img :src="$options.imageConfig.BextCar.carro" alt="um carro branco sobre um fundo azul"
-          class="sm:w-full md:max-w-[30rem] lg:max-w-3xl max-w-4xl h-auto  xl:sticky xl:mt-[-35%] z-0" />
+        <img
+          :src="$options.imageConfig.BextCar.carro"
+          alt="um carro branco sobre um fundo azul"
+          class="sm:w-full md:max-w-[30rem] lg:max-w-3xl max-w-4xl h-auto xl:sticky xl:mt-[-35%] z-0" />
       </div>
-      <div class="w-1/2 sm:w-full text-right mt-auto sm:pt-12 mb-auto text-white">
-        <h1 class="xl:w-2/3 lg:w-2/3 md:w-full sm:text-center sm:text-3xl text-4xl md:text-3xl font-semibold
-              text-right mr-0 ml-auto">
+      <div
+        class="w-1/2 sm:w-full text-right mt-auto sm:pt-12 mb-auto text-white">
+        <h1
+          class="xl:w-2/3 lg:w-2/3 md:w-full sm:text-center sm:text-3xl text-4xl md:text-3xl font-semibold text-right mr-0 ml-auto">
           Como Funciona?
         </h1>
-        <p class="py-6 pl-[12.5rem] sm:pl-0  sm:text-center lg:pl-24 md:pl-8 text-xl xl:text-2xl md:text-lg ">
-          Com o BextCar, você pode usar o seu <span class="font-bold ">carro,
-          van ou caminhão quitado para conseguir empréstimo rápido.</span>
+        <p
+          class="py-6 pl-[12.5rem] sm:pl-0 sm:text-center lg:pl-24 md:pl-8 text-xl xl:text-2xl md:text-lg">
+          Com o BextCar, você pode usar o seu
+          <span class="font-bold">
+            carro, van ou caminhão quitado para conseguir empréstimo rápido.
+          </span>
           Nessa modalidade,
-          <span class="font-bold"> as  taxas são mais baixas,</span>
+          <span class="font-bold"> as taxas são mais baixas,</span>
           já que seu veículo é usado como uma garantia.
         </p>
       </div>
     </div>
-    <banner />
+    <Banner />
     <div class="maxWidth">
-      <card />
+      <Card />
     </div>
   </section>
 </template>
 
 <script lang="ts">
-
-import { defineComponent } from 'vue';
-import card from '@/components/vehicle/BextCarLP/card.vue'
-import banner from '@/components/vehicle/BextCarLP/banner.vue'
-export default defineComponent({
-  name: 'Section2',
-  components: {
-    card,
-    banner
-  }
-})
+  import { defineComponent } from 'vue';
+  import card from '@/components/vehicle/BextCarLP/card.vue';
+  import banner from '@/components/vehicle/BextCarLP/banner.vue';
+  export default defineComponent({
+    name: 'Section2',
+    components: {
+      card,
+      banner,
+    },
+  });
 </script>
 
 <style></style>
